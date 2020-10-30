1c926f333684a92ceeecb5660014853179582199
Trung Hiếu <45393995+hakunhan@users.noreply.github.com>
2020-10-30 19:02:59 +0700

    Add to branch

---
+++ src/Account.java
@@ -0,0 +1,9 @@
+public class Account {
+    private int EmployeeID;
+    private String username;
+    private String password;
+
+    public Account(int EmployeeID,String username, String password){
+
+    }
+}
---
+++ src/Salary.java
@@ -0,0 +1,5 @@
+public class Salary {
+    private int WorkTimetable_id;
+    private WorkTimetable workTimetable;
+    private float hourlyRate;
+}
