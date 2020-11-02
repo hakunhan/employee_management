a14da7a7a58acc7d23eea4335af39c7b0a9e4263
Le Duc Long <62542108+lelongrvp@users.noreply.github.com>
2020-11-02 20:16:37 +0700

    Add files via upload

--- src/Account.java
+++ src/Account.java
@@ -4,6 +4,33 @@ public class Account {
     private String password;
 
     public Account(int EmployeeID,String username, String password){
-
+    	this.EmployeeID = EmployeeID;
+    	this.username = username;
+    	this.password = password
     }
+
+    public int getEmployeeID{
+    	return EmployeeID;
+    }
+
+    public void setEmployeeID{
+    	this.EmployeeID = EmployeeID;
+    }
+
+    public String getUsername{
+    	return username;
+    }
+
+    public void setUsername{
+    	this.username = username;
+    }
+
+    public String getPassword{
+    	return password;
+    }
+
+    public void setPassword{
+    	this.password = password;
+    }
+
 }
