a926366c7d7fe9a661ab28f6ff048ca782949937
Trung Hiếu <45393995+hakunhan@users.noreply.github.com>
2020-11-13 18:01:20 +0700

    Solving connection to database

--- src/Database/DBUtils.java
+++ src/Database/DBUtils.java
@@ -3,23 +3,37 @@ package Database;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DBUtils {
+    private String url;
+    private String user;
+    private String password;
+    private Connection connection;
+
     public void initialize(){
         Properties p = new Properties();
         try{
             p.load(new FileInputStream("src\\Database\\database.properties"));
-            String url = p.getProperty("url");
-            String user = p.getProperty("user");
-            String password = p.getProperty("password");
-            System.out.println(url);
-            System.out.println(user);
-            System.out.println(password);
-        } catch (FileNotFoundException e) {
+            url = p.getProperty("url");
+            user = p.getProperty("user");
+            password = p.getProperty("password");
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url + "?user=" + user+ "&password=" + password
+                    + "&useUnicode=true&characterEncoding=UTF-8");
+        } catch (ClassNotFoundException | SQLException | IOException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        }
+
+        if (connection == null){
+            throw new NullPointerException("Connection is null");
+        }
+        else {
+            System.out.println("Connected");
         }
     }
 
