d31da01fdfec0f893d688283bd5ec8e7576dd4a8
Trung Hiếu <45393995+hakunhan@users.noreply.github.com>
2020-11-14 18:31:17 +0700

    Update insert and update table

--- src/Database/WorkTimeTableSqlStatement.java
+++ src/Database/WorkTimeTableSqlStatement.java
@@ -13,59 +13,61 @@ public class WorkTimeTableSqlStatement {
         dbUtils.insertData(mysqlStatement,indexes,values);
     }
 
-    public void updateMonday(String oldValues, String newValue){
-        String mysqlStatement = "UPDATE workTimeTable SET monday=? WHERE monday=?;";
+    public void updateMonday(String employee_id, String newValue){
+        String mysqlStatement = "UPDATE workTimeTable SET monday=? WHERE employee_id=?;";
         int[] indexes = {1,2};
-        String[] values = {newValue,oldValues};
+        String[] values = {newValue, employee_id};
 
         dbUtils.updateData(mysqlStatement,indexes,values);
     }
 
-    public void updateTuesday(String oldValues, String newValue){
-        String mysqlStatement = "UPDATE workTimeTable SET tuesday=? WHERE tuesday=?;";
+    public void updateTuesday(String employee_id, String newValue){
+        String mysqlStatement = "UPDATE workTimeTable SET tuesday=? WHERE employee_id=?;";
         int[] indexes = {1,2};
-        String[] values = {newValue,oldValues};
+        String[] values = {newValue,employee_id};
 
         dbUtils.updateData(mysqlStatement,indexes,values);
     }
 
-    public void updateWednesday(String oldValues, String newValue){
-        String mysqlStatement = "UPDATE workTimeTable SET wednesday=? WHERE wednesday=?;";
+    public void updateWednesday(String employee_id, String newValue){
+        String mysqlStatement = "UPDATE workTimeTable SET wednesday=? WHERE employee_id=?;";
         int[] indexes = {1,2};
-        String[] values = {newValue,oldValues};
+        String[] values = {newValue,employee_id};
 
         dbUtils.updateData(mysqlStatement,indexes,values);
     }
 
-    public void updateThursday(String oldValues, String newValue){
-        String mysqlStatement = "UPDATE workTimeTable SET thursday=? WHERE thursday=?;";
+    public void updateThursday(String employee_id, String newValue){
+        String mysqlStatement = "UPDATE workTimeTable SET thursday=? WHERE employee_id=?;";
         int[] indexes = {1,2};
-        String[] values = {newValue,oldValues};
+        String[] values = {newValue,employee_id};
 
         dbUtils.updateData(mysqlStatement,indexes,values);
     }
 
-    public void updateFriday(String oldValues, String newValue){
-        String mysqlStatement = "UPDATE workTimeTable SET friday=? WHERE friday=?;";
+    public void updateFriday(String employee_id, String newValue){
+        String mysqlStatement = "UPDATE workTimeTable SET friday=? WHERE employee_id=?;";
         int[] indexes = {1,2};
-        String[] values = {newValue,oldValues};
+        String[] values = {newValue,employee_id};
 
         dbUtils.updateData(mysqlStatement,indexes,values);
     }
 
-    public void updateSaturday(String oldValues, String newValue){
-        String mysqlStatement = "UPDATE workTimeTable SET saturday=? WHERE saturday=?;";
+    public void updateSaturday(String employee_id, String newValue){
+        String mysqlStatement = "UPDATE workTimeTable SET saturday=? WHERE employee_id=?;";
         int[] indexes = {1,2};
-        String[] values = {newValue,oldValues};
+        String[] values = {newValue,employee_id};
 
         dbUtils.updateData(mysqlStatement,indexes,values);
     }
 
-    public void updateSunday(String oldValues, String newValue){
-        String mysqlStatement = "UPDATE workTimeTable SET sunday=? WHERE sunday=?;";
+    public void updateSunday(String employee_id, String newValue){
+        String mysqlStatement = "UPDATE workTimeTable SET sunday=? WHERE employee_id=?;";
         int[] indexes = {1,2};
-        String[] values = {newValue,oldValues};
+        String[] values = {newValue,employee_id};
 
         dbUtils.updateData(mysqlStatement,indexes,values);
     }
+
+    public void deleteWorkTimeTable
 }
